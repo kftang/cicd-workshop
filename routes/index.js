36439663f007a465ce68b29a568eec5de07e9b61
kftang <kennyftang@gmail.com>
2020-10-07 19:34:01 -0500

    Implemented multiply route

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,14 @@ router.get('/add', (req, res) => {
   res.status(400).send('Invalid query');
 });
 
+router.get('/multiply', (req, res) => {
+  const { a, b } = req.query;
+  if (a && b) {
+    const aNumber = Number(a);
+    const bNumber = Number(b);
+    return res.json({ value: aNumber * bNumber })
+  }
+  res.status(400).send('Invalid query');
+});
+
 module.exports = router;
